e7dd0013059093094b6919b5178513d2430c5f4d
Jonischkeit Clemens <jonischk@sec.in.tum.de>
2019-03-18 11:34:58 +0100

    added socket wrapper to grab flags

--- pwnutils.py
+++ pwnutils.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import socket
+import re
+
 def recv_until(sock, delim):
 	tmp = b''
 	while delim not in tmp:
@@ -22,6 +25,38 @@ def recv_all(sock):
 			tmp += r
 		except InterruptedError:
 			pass
-		except:
-			break
+#		except:
+#			break
 	return tmp
+
+class FlagSock(socket.socket):
+	flagbuf = b""
+	flagbuflen = 100
+	flagfile = "flags.txt"
+	flagregex = b"hxp{[^}]+}"
+
+	def __init__(self, a = None, b = None):
+		if a is None:
+			super(FlagSock, self).__init__()
+		elif b is None:
+			super(FlagSock, self).__init__(a)
+		else:
+			super(FlagSock, self).__init__(a,b)
+
+	def flagregexfun(self, inp):
+		res = re.search(self.flagregex, inp)
+		return (res.end(), res.group()) if res is not None else None
+
+	flagfun = flagregexfun
+
+	def recv(self, bufsize, flags = 0):
+		tmp = super(FlagSock, self).recv(bufsize, flags)
+		self.flagbuf += tmp
+		res = self.flagfun(self.flagbuf)
+		if res is not None:
+			print("Flag: " + str(res[1]))
+			self.flagbuf = self.flagbuf[res[0]:]
+			with open(self.flagfile, "ab") as ff:
+				ff.write(res[1])
+		self.flagbuf = self.flagbuf[:self.flagbuflen]
+		return tmp
